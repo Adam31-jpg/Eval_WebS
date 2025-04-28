a95a52aef4bfe3488136c20df29d533e144fc918
Pierre RUIBANYS <piere@placeme.io>
2025-04-28 13:39:37 +0200

    update docker-compose

--- api-graphql/src/entities/reservation.entity.ts
+++ api-graphql/src/entities/reservation.entity.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import {
   BeforeInsert,
   BeforeUpdate,
@@ -16,18 +15,10 @@ import { UserEntity } from './user.entity';
 @Entity('reservations')
 export class ReservationEntity {
   @PrimaryGeneratedColumn()
-  @ApiProperty({
-    description: 'ID unique de la réservation',
-    example: '1',
-  })
   id: string;
 
   @Column({
     type: 'int',
-  })
-  @ApiProperty({
-    description: "ID de l'utilisateur qui a fait la réservation",
-    example: 1,
   })
   userId: number;
 
@@ -36,43 +27,22 @@ export class ReservationEntity {
     onDelete: 'CASCADE',
     onUpdate: 'CASCADE',
   })
-  @ApiProperty({
-    description: "Données de l'utilisateur",
-    type: () => UserEntity,
-  })
   user: UserEntity;
 
   @Column({
     type: 'int',
   })
-  @ApiProperty({
-    description: 'ID de la chambre réservée',
-    example: 1,
-  })
   roomId: number;
 
   @OneToMany(() => RoomEntity, (room) => room.id)
-  @ApiProperty({
-    description: 'Données de la chambre',
-    type: () => RoomEntity,
-    required: false,
-  })
   room?: RoomEntity;
 
   @Column({ type: 'varchar' })
-  @ApiProperty({
-    description: 'Localisation de la réservation',
-    example: 'Bâtiment A, Paris',
-  })
   location: string;
 
   @CreateDateColumn({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP(6)',
-  })
-  @ApiProperty({
-    description: 'Date de création de la réservation',
-    example: '2025-03-18T10:30:00Z',
   })
   createdAt: Date;
 
@@ -85,25 +55,11 @@ export class ReservationEntity {
   }
 
   @Column({ type: 'timestamp' })
-  @ApiProperty({
-    description: 'Date et heure de début de la réservation',
-    example: '2025-04-01T14:00:00Z',
-  })
   startTime: Date;
 
   @Column({ type: 'timestamp' })
-  @ApiProperty({
-    description: 'Date et heure de fin de la réservation',
-    example: '2025-04-01T16:00:00Z',
-  })
   endTime: Date;
 
   @Column({ type: 'varchar' })
-  @ApiProperty({
-    description: 'Statut de la réservation',
-    enum: StatusEnum,
-    enumName: 'StatusEnum',
-    example: StatusEnum.APPROVED,
-  })
   status: StatusEnum;
 }
